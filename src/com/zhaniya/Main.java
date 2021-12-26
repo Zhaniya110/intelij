3251c686379a41769a0446d747c9595250086c97
Zhaniya <zmeyramova@gmail.com>
2021-12-26 13:37:12 +0600

    So what is that ? Git commit and push I guess

---
+++ src/com/zhaniya/Main.java
@@ -0,0 +1,13 @@
+package com.zhaniya;
+
+
+// c:\Wnidows\...
+public class Main {
+
+    public static void main(String[] args) {
+    int[] numbers = new int[5];
+    numbers[0]=1;
+    numbers[1]=2;
+    numbers[10] = 3    ;
+  }
+    }
